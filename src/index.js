f06ef5c6800ac08cb4fc63fcdf31cdb5ce5baaf3
Diego-Tello <diegotellomendoza@gmail.com>
2025-11-23 21:44:55 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+window.onload = () => {
+let who = ['Mi perro', 'Mi abuela', 'El cartero', 'Un pajarito'];
+let action = ['se comió', 'mojó', 'mordió', 'rompió', 'perdió', 'me robó'];
+let what = ['mi tarea', 'mi móvil', 'el carro','las llaves'];
+let when = ['antes de clase', 'cuando estaba durmiendo', 'mientras hacia ejercicio', 'mientras comía'];
+
+const btnGenerater=document.querySelector('.btn');
+console.log(btnGenerater);
+btnGenerater.addEventListener('click',() => {
+
+    function RandomElement(array){
+      return Math.floor(Math.random(array)*array.length);
+    }
+
+    let RandomWho= who[RandomElement(who)]
+    let RandomAction= action[RandomElement(action)]
+    let RandomWhat= what[RandomElement(what)]
+    let RandomWhen= when[RandomElement(when)]
+
+    let GeneradorExcusa= `${RandomWho} ${RandomAction} ${RandomWhat} ${RandomWhen}`;
+
+    document.querySelector('.excusa').innerHTML=GeneradorExcusa;
+
+});
+
+}
+
+
+
+
+
